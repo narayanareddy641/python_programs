305f1fa26053874c875f86a572464486f6591b13
Narayana Reddy <nmreddy641@gmail.com>
2020-01-13 23:21:52 +0530

    Update random_check.py

--- random_check.py
+++ random_check.py
@@ -3,3 +3,13 @@ def rand_check(num,low,high):
         print('{} is range between {} and {}'.format(num,low,high))
     else:
         print('The number is out of range')
+
+        
+        
+        
+        =====================================
+        
+        Boolean check 
+
+def rand_bool(num,low,high):
+    return num in range(low,high+1)
