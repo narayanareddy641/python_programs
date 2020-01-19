9cdeba0cff93792b9c84ff49d3c39373380dce22
Narayana Reddy <nmreddy641@gmail.com>
2020-01-19 12:22:04 +0530

    print pattern program

---
+++ patternprogram.py
@@ -0,0 +1,19 @@
+for i in range(4+1):
+    for j in range(i):
+        print("# ",end=' ')
+        
+    print()
+    
+    
+    
+    
+ =================================================
+ 
+ 
+# patten with decending order
+
+for i in range(4+1):
+    for j in range(4-i):
+        print("# ",end=' ')
+        
+    print()
